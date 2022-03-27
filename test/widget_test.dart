8ddd2001d297fe02e0d48d1cfecc442daecab6c0
Kanata Miyahana <afiri.bupa@gmail.com>
2022-03-27 16:05:54 +0900

    add test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,25 +6,15 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_myfirstapp/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_myfirstapp/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('Start screen is shown', (WidgetTester tester) async {
+    await tester.binding.setSurfaceSize(const Size(400, 800));
     await tester.pumpWidget(const MyFirstApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Slide Puzzle'), findsOneWidget);
+    expect(find.text('Start'), findsOneWidget);
   });
 }
